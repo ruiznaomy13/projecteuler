3c8b8e67a0454ba7d9ae62ab8b687add81664409
ruiznaomy13 <ruiznaomy13@gmail.com>
2025-12-24 17:50:34 +0100

    problem 000

--- solutions/problem_000.c
+++ solutions/problem_000.c
@@ -3,22 +3,24 @@
 
 /*
  * @precondition  n > 0
- * @postcondition Returns 1² + 3² + 5² + ... + (2n − 1)² = n(2n − 1)(2n + 1) / 3
+ * @postcondition Returns:
+ *   1² + 3² + 5² + ... + (2n - 1)²
+ *   = n(2n - 1)(2n + 1) / 3
  */
-long long sum_of_odd_squares(int n)
+long sum_of_odd_squares(long n)
 {
     assert(n > 0);
 
-    long long two_n = 2 * n;
+    long two_n = 2 * n;
     return (n * (two_n - 1) * (two_n + 1)) / 3;
 }
 
 int main(void)
 {
-    const int LIMIT = 531000 / 2;
+    const long limit = 531000 / 2;
 
-    long long result = sum_of_odd_squares(LIMIT);
-    printf("%lld\n", result);
+    long result = sum_of_odd_squares(limit);
+    printf("%ld\n", result);
 
     return 0;
 }
