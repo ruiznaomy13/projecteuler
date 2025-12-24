db98ae6f0fac4a899b55de2bb1400b77b5f5cb73
ruiznaomy13 <ruiznaomy13@gmail.com>
2025-12-24 17:50:56 +0100

    problem 001

--- solutions/problem_001.c
+++ solutions/problem_001.c
@@ -3,22 +3,24 @@
 
 /*
  * @precondition  n > 0
- * @postcondition Returns 1 + 2 + 3 + ... + n = n(n + 1) / 2
+ * @postcondition Returns:
+ *   1 + 2 + 3 + ... + n = n(n + 1) / 2
  */
 int sum_of_n(int n)
 {
     assert(n > 0);
-
-    return (n * (n + 1)) / 2;
+    return n * (n + 1) / 2;
 }
 
 int main(void)
 {
-    const int LIMIT = 1000 - 1;
-    const int sum_mul_of_three = 3 * sum_of_n(LIMIT / 3);
-    const int sum_mul_of_five = 5 * sum_of_n(LIMIT / 5);
-    const int sum_mul_of_fifteen = 15 * sum_of_n(LIMIT / 15);
-    int       result = sum_mul_of_three + sum_mul_of_five - sum_mul_of_fifteen;
+    const int max = 999;
+
+    int sum_3 = 3 * sum_of_n(max / 3);
+    int sum_5 = 5 * sum_of_n(max / 5);
+    int sum_15 = 15 * sum_of_n(max / 15);
+
+    int result = sum_3 + sum_5 - sum_15;
     printf("%d\n", result);
 
     return 0;
